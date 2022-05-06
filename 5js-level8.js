1d713f0a9ef6a339181d56ca100b5a5ee795bedb
EdmondDantess <maximlavrovsky@mail.ru>
2022-05-06 21:55:42 +0300

    done

---
+++ 5js-level8.js
@@ -0,0 +1,18 @@
+/*Convert number to reversed array of digits
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+Example:
+348597 => [7,9,5,8,4,3]
+0 => [0] */
+
+function digitize(n) {
+let numArray = n.toString().split('');
+const result = numArray.reverse();
+resMassive = result.toString().split(',')
+a = resMassive.map(element => 
+   parseInt(element));
+    console.log(a)
+    return a
+}
+
+digitize(1325325)
