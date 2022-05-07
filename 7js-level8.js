dda8a066c6fde72db916d3afda7106336ff9c5f2
EdmondDantess <maximlavrovsky@mail.ru>
2022-05-07 19:28:36 +0300

    done

---
+++ 7js-level8.js
@@ -0,0 +1,24 @@
+/*
+If you can't sleep, just count sheep!!
+
+Task:
+Given a non-negative integer, 3 for example, return a string 
+with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.*/
+
+
+
+
+
+var countSheep = function (num) {
+    let arr = [];
+    for (let i = 0; i < num; i++) {
+        res = i + 1 + ' sheep...';
+        arr[i] = res;
+
+    }
+    
+    arr = arr.join('').toString();
+    console.log(arr);
+}
+
+countSheep(34)
