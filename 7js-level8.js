80e0762f0a1cec1533b19fa0f69831de50c78554
EdmondDantess <maximlavrovsky@mail.ru>
2022-05-10 12:52:36 +0300

    done

--- 7js-level8.js
+++ 7js-level8.js
@@ -11,14 +11,17 @@ with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e
 
 var countSheep = function (num) {
     let arr = [];
+    if (num == 0) {
+        return ""
+    } else {
     for (let i = 0; i < num; i++) {
         res = i + 1 + ' sheep...';
         arr[i] = res;
-
+    }
     }
     
     arr = arr.join('').toString();
     console.log(arr);
 }
 
-countSheep(34)
+countSheep(22)
